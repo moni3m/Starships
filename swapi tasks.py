4f4505078621063c4f0b5c19b63ba2f61dbc240f
moni3m <moniem.ibrahim@outlook.com>
2022-07-06 13:39:07 +0100

    first push

---
+++ swapi tasks.py
@@ -0,0 +1,43 @@
+# ---------------------------------TASK--------------------------------------------------------------
+
+# The data in this database has been pulled from https://swapi.dev/. As well as 'people', the API has data on starships.
+# In Python, pull data on all available starships from the API. The "pilots" key contains URLs pointing to the characters who pilot the starship.
+# Use these to replace 'pilots' with a list of ObjectIDs from our characters collection,
+# then insert the starships into their own collection.
+# Use functions at the very least!
+
+# ---------------------PLAN----------------------------------
+
+# Create a function that will get starships data from the API
+# Create a function that will loop through number of pages from the API collecting starship data from each page
+# Create a function that checks if pilots are empty and retrieve the pilot's API call from each starship
+# Create a function that takes searches for the corresponding pilot ID from the pilot API call
+# Create a function that replaces the pilot API call in the starships to the pilot ID
+# Create a function that inserts the starships data with the new information into a new collection
+
+# --------------------IMPORTING LIBRARIES---------------------
+
+import pymongo
+import requests
+from pprint import pprint
+
+# ---------------------FUNCTIONS---------------------------
+
+
+def do_api_call(url: str):  # This function completes the API call request by inputting the URL as the arguement
+    results = requests.get(url).json()  # HTTP GET request from the given URL returning the results in a JSON format, storing it in a varibale called "response"
+    return results  # returns only the result\'s dictionary from the API call back to the function
+
+
+#pprint(do_api_call("https://swapi.dev/api/starships/?page=4"))
+
+
+def collecting_ship_data():  # This function will loop through the number of pages and collecting the ship data from each page
+    url = f"https://swapi.dev/api/starships/?page=1"  # this is the URL of the first page
+    while requests.get(url).status_code == 200:  # a WHILE loop where condition is that the request is successful
+        results = requests.get(url).json()["results"]
+        new_page = requests.get(url).json()["next"]
+        url = new_page
+        pprint(results)
+
+collecting_ship_data()
